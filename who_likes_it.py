dfdd3c133f9cbbcc2e08cfe1ca5e15d61ef4bf99
Daniel Burgess <danieljbburgess@hotmail.co.uk>
2023-04-25 20:28:25 +0100

    Solution for Who likes it

---
+++ who_likes_it.py
@@ -0,0 +1,28 @@
+# Kata Link: https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+
+name0 = []
+name1 = ["Peter"]
+name2 = ["Jacob", "Alex"]
+name3 = ["Max", "John", "Mark"]
+name4 = ["Alex", "Jacob", "Mark", "Max"]
+name5 = ["Alex", "Jacob", "Mark", "Max", "Daniel"]
+
+name_lists = [name0, name1, name2, name3, name4, name5]
+
+
+def likes(names):
+    match len(names):
+        case 0:
+            return "no one likes this"
+        case 1:
+            return names[0] + " likes this"
+        case 2:
+            return names[0] + " and " + names[1] + " like this"
+        case 3:
+            return names[0] + ", " + names[1] + " and " + names[2] + " like this"
+        case _:
+            return names[0] + ", " + names[1] + " and " + str(len(names) - 2) + " others like this"
+
+
+for name_list in name_lists:
+    print(likes(name_list))

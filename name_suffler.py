757928ba5c44ad289b6af690115ca190aa8bc0cd
Daniel Burgess <danieljbburgess@hotmail.co.uk>
2023-06-02 14:49:29 +0100

    Added Codewars example tests and added its requirements

--- name_suffler.py
+++ name_suffler.py
@@ -1,10 +1,17 @@
 # Kata link: https://www.codewars.com/kata/559ac78160f0be07c200005a
 
+import codewars_test as test
+
 names = ['john McClane','Mary jeggins','tom jerry']
 
 def name_shuffler(str_):
     split = str_.split()
     return split[1] + " " + split[0]
 
-for name in names:
-    print(name_shuffler(name))
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(name_shuffler('john McClane'),'McClane john')
+        test.assert_equals(name_shuffler('Mary jeggins'),'jeggins Mary')
+        test.assert_equals(name_shuffler('tom jerry'),'jerry tom')
